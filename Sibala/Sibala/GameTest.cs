a8ac371e1bbd01cfcd3f68bf1feb715c479e7e19
Yu-Lun <chou522128@gmail.com>
2023-02-11 22:54:32 +0800

    [Refactor] GameTest.cs：add SetUp function
    use the private _game to define a variable

--- Sibala/Sibala/GameTest.cs
+++ Sibala/Sibala/GameTest.cs
@@ -6,11 +6,18 @@ namespace Sibala
     [TestFixture]
     public class GameTest
     {
+        private Game _game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _game = new Game();
+        }
+
         [Test]
         public void A01_BothAllOfKind()
         {
-            var game = new Game();
-            var actual = game.ShowResult("Black: 6 6 6 6  White: 3 3 3 3");
+            var actual = _game.ShowResult("Black: 6 6 6 6  White: 3 3 3 3");
             actual.Should().Be("Black win. - with all of a kind: 6");
         }
     }
