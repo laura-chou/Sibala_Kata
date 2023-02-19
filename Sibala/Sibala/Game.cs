794c6bc24067981a52238f147a68c0ef9d74dc99
Yu-Lun <chou522128@gmail.com>
2023-02-19 16:05:00 +0800

    [Green] GameTest.cs - A01_BothAllOfKind：add pointOrder list
    input：Black: 4 4 4 4  White: 6 6 6 6
    output：Black win. - with all of a kind: 4
    add pointOrder list and compare players point with index value from the pointOrder list

--- Sibala/Sibala/Game.cs
+++ Sibala/Sibala/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Sibala
@@ -15,9 +16,10 @@ namespace Sibala
 
             if (player1Point != player2Point)
             {
-                var winnerPlayer = (player1Point > player2Point) ? player[0].Name : player[1].Name;
+                var pointOrder = new List<int> { 2, 3, 5, 6, 4, 1 };
+                var winnerPlayer = (pointOrder.IndexOf(player1Point) > pointOrder.IndexOf(player2Point)) ? player[0].Name : player[1].Name;
                 var winnerCategory = "all of a kind";
-                var winnerPoints = (player1Point > player2Point) ? player1Point : player2Point;
+                var winnerPoints = (pointOrder.IndexOf(player1Point) > pointOrder.IndexOf(player2Point)) ? player1Point : player2Point;
                 return $"{winnerPlayer} win. - with {winnerCategory}: {winnerPoints}";
             }
 
