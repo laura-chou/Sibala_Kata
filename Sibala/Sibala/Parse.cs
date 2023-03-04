d4780fcaf4af2d3cff87efa667acb3e4a6f31520
Yu-Lun <chou522128@gmail.com>
2023-03-04 22:35:19 +0800

    [Refactor] Parse.cs：split input string to get players dices

--- Sibala/Sibala/Parse.cs
+++ Sibala/Sibala/Parse.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Sibala
 {
@@ -10,32 +11,29 @@ namespace Sibala
         {
             var player1Section = input.Split("  ", StringSplitOptions.RemoveEmptyEntries)[0];
             var player1Name = player1Section.Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
+            var player1Dices = player1Section.Split(":", StringSplitOptions.RemoveEmptyEntries)[1]
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => new Dice { Value = int.Parse(s), Output = s })
+                .ToList();
+
             var player2Section = input.Split("  ", StringSplitOptions.RemoveEmptyEntries)[1];
             var player2Name = player2Section.Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
+            var player2Dices = player2Section.Split(":", StringSplitOptions.RemoveEmptyEntries)[1]
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => new Dice { Value = int.Parse(s), Output = s })
+                .ToList();
 
             return new List<Player>
             {
                 new Player
                 {
                     Name = player1Name,
-                    Dices = new List<Dice>
-                    {
-                        new Dice{ Value = 6, Output = "6"},
-                        new Dice{ Value = 6, Output = "6"},
-                        new Dice{ Value = 6, Output = "6"},
-                        new Dice{ Value = 6, Output = "6"}
-                    }
+                    Dices = new List<Dice>(player1Dices)
                 },
                 new Player
                 {
                     Name = player2Name,
-                    Dices = new List<Dice>
-                    {
-                        new Dice{ Value = 3, Output = "3"},
-                        new Dice{ Value = 3, Output = "3"},
-                        new Dice{ Value = 3, Output = "3"},
-                        new Dice{ Value = 3, Output = "3"}
-                    }
+                    Dices = new List<Dice>(player2Dices)
                 }
             };
         }
