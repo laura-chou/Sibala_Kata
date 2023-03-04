422b36ea0612ac2bd55c060582696268a50afac8
Yu-Lun <chou522128@gmail.com>
2023-03-04 22:23:23 +0800

    [Refactor] Parse.cs：split input string to get players name

--- Sibala/Sibala/Parse.cs
+++ Sibala/Sibala/Parse.cs
@@ -8,15 +8,20 @@ namespace Sibala
     {
         public List<Player> Parser(string input)
         {
+            var player1Section = input.Split("  ", StringSplitOptions.RemoveEmptyEntries)[0];
+            var player1Name = player1Section.Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
+            var player2Section = input.Split("  ", StringSplitOptions.RemoveEmptyEntries)[1];
+            var player2Name = player2Section.Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
+
             return new List<Player>
             {
                 new Player
                 {
-                    Name = "Black",
+                    Name = player1Name,
                 },
                 new Player
                 {
-                    Name = "White",
+                    Name = player2Name,
                 }
             };
         }
