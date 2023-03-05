a648d622a1fee7e5c9308b9cb2902495dc9c9178
Yu-Lun <chou522128@gmail.com>
2023-03-05 22:12:42 +0800

    [Green] Game.cs - A01_BothAllOfKind：add diceOrder list
    compare players dice with index value from the diceOrder list

--- Sibala/Sibala/Game.cs
+++ Sibala/Sibala/Game.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Sibala
@@ -13,11 +15,14 @@ namespace Sibala
             var player1Dice = parser[0].Dices.First().Value;
             var player2Dice = parser[1].Dices.First().Value;
 
+            var diceOrder = new List<int> { 2, 3, 5, 6, 4, 1 };
+            var compareDice = diceOrder.IndexOf(player1Dice) > diceOrder.IndexOf(player2Dice);
+            
             if (player1Dice != player2Dice)
             {
-                var winnerPlayer = (player1Dice > player2Dice) ? parser[0].Name : parser[1].Name;
+                var winnerPlayer = compareDice ? parser[0].Name : parser[1].Name;
                 var winnerCategory = "all of a kind";
-                var winnerPoint = (player1Dice > player2Dice) ? player1Dice : player2Dice;
+                var winnerPoint = compareDice ? player1Dice : player2Dice;
 
                 return $"{winnerPlayer} win. - with {winnerCategory}: {winnerPoint}";
             }
