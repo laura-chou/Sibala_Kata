491f31653b5cf2dae5c31c481a4e2e097b1a6579
Yu-Lun <chou522128@gmail.com>
2023-03-05 22:01:05 +0800

    [Green] Game.cs - A01_BothAllOfKind：compare players dices point
    compare players dices value, then get winner name and point

--- Sibala/Sibala/Game.cs
+++ Sibala/Sibala/Game.cs
@@ -9,9 +9,14 @@ namespace Sibala
         {
             var parse = new Parse();
             var parser = parse.Parser(input);
-            var winnerPlayer = parser[0].Name;
+
+            var player1Dice = parser[0].Dices.First().Value;
+            var player2Dice = parser[1].Dices.First().Value;
+
+            var winnerPlayer = (player1Dice > player2Dice) ? parser[0].Name : parser[1].Name;
             var winnerCategory = "all of a kind";
-            var winnerPoint = parser[0].Dices.First().Value;
+            var winnerPoint = (player1Dice > player2Dice) ? player1Dice : player2Dice;
+
             return $"{winnerPlayer} win. - with {winnerCategory}: {winnerPoint}";
         }
     }
