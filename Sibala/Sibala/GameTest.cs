cfdca3087b4a3e1af66c8f208558c099f7de2c79
Yu-Lun <chou522128@gmail.com>
2023-03-05 22:14:17 +0800

    [Green] GameTest.cs - A01_BothAllOfKind：add test case
    input：Black: 6 6 6 6  White: 4 4 4 4
    output：White win. - with all of a kind: 4

--- Sibala/Sibala/GameTest.cs
+++ Sibala/Sibala/GameTest.cs
@@ -19,6 +19,7 @@ namespace Sibala
         [TestCase("Black: 2 2 2 2  White: 5 5 5 5", "White win. - with all of a kind: 5")]
         [TestCase("Black: 2 2 2 2  White: 2 2 2 2", "Tie")]
         [TestCase("Black: 4 4 4 4  White: 6 6 6 6", "Black win. - with all of a kind: 4")]
+        [TestCase("Black: 6 6 6 6  White: 4 4 4 4", "White win. - with all of a kind: 4")]
         public void A01_BothAllOfKind(string input, string expected)
         {
             var actual = _game.ShowResult(input);
