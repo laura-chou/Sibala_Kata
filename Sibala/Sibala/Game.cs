131c430df5dce364c544e445a6214bdeeec730c9
Yu-Lun <chou522128@gmail.com>
2023-03-05 22:05:03 +0800

    [Green] Game.cs - A01_BothAllOfKind：check players point is the same
    if players point is the same  return Tie

--- Sibala/Sibala/Game.cs
+++ Sibala/Sibala/Game.cs
@@ -13,11 +13,16 @@ namespace Sibala
             var player1Dice = parser[0].Dices.First().Value;
             var player2Dice = parser[1].Dices.First().Value;
 
-            var winnerPlayer = (player1Dice > player2Dice) ? parser[0].Name : parser[1].Name;
-            var winnerCategory = "all of a kind";
-            var winnerPoint = (player1Dice > player2Dice) ? player1Dice : player2Dice;
+            if (player1Dice != player2Dice)
+            {
+                var winnerPlayer = (player1Dice > player2Dice) ? parser[0].Name : parser[1].Name;
+                var winnerCategory = "all of a kind";
+                var winnerPoint = (player1Dice > player2Dice) ? player1Dice : player2Dice;
 
-            return $"{winnerPlayer} win. - with {winnerCategory}: {winnerPoint}";
+                return $"{winnerPlayer} win. - with {winnerCategory}: {winnerPoint}";
+            }
+
+            return "Tie";
         }
     }
 }
