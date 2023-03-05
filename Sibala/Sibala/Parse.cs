1042cf6c003abd098f57c6f08f405af4cc0669fd
Yu-Lun <chou522128@gmail.com>
2023-03-05 21:50:54 +0800

    [Refactor] Parse.cs：add GetPlayer function
    refactor code to GetPlayer function

--- Sibala/Sibala/Parse.cs
+++ Sibala/Sibala/Parse.cs
@@ -9,32 +9,25 @@ namespace Sibala
     {
         public List<Player> Parser(string input)
         {
-            var player1Section = input.Split("  ", StringSplitOptions.RemoveEmptyEntries)[0];
-            var player1Name = player1Section.Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
-            var player1Dices = player1Section.Split(":", StringSplitOptions.RemoveEmptyEntries)[1]
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                .Select(s => new Dice { Value = int.Parse(s), Output = s })
-                .ToList();
+            var playerSection = input.Split("  ", StringSplitOptions.RemoveEmptyEntries);
 
-            var player2Section = input.Split("  ", StringSplitOptions.RemoveEmptyEntries)[1];
-            var player2Name = player2Section.Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
-            var player2Dices = player2Section.Split(":", StringSplitOptions.RemoveEmptyEntries)[1]
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                .Select(s => new Dice { Value = int.Parse(s), Output = s })
+            return playerSection
+                .Select(s => GetPlayer(s))
                 .ToList();
+        }
 
-            return new List<Player>
+        private Player GetPlayer(string playerSection)
+        {
+            var playerName = playerSection.Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
+            var playerDices = playerSection.Split(":", StringSplitOptions.RemoveEmptyEntries)[1]
+                            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                            .Select(s => new Dice { Value = int.Parse(s), Output = s })
+                            .ToList();
+
+            return new Player
             {
-                new Player
-                {
-                    Name = player1Name,
-                    Dices = new List<Dice>(player1Dices)
-                },
-                new Player
-                {
-                    Name = player2Name,
-                    Dices = new List<Dice>(player2Dices)
-                }
+                Name = playerName,
+                Dices = new List<Dice>(playerDices)
             };
         }
     }
