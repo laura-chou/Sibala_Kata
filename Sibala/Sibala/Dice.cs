2642a45936cae2968d91ad1d91026ec4d0459db1
Yu-Lun <chou522128@gmail.com>
2023-03-18 22:38:47 +0800

    reset

--- Sibala/Sibala/Dice.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sibala
-{
-    public class Dice
-    {
-        public int Value { get; internal set; }
-        public string Output { get; internal set; }
-    }
-}
--- Sibala/Sibala/Player.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-namespace Sibala
-{
-    public class Player
-    {
-        public string Name { get; set; }
-        public List<Dice> Dices { get; set; }
-    }
-}
