59a82760b5f8fc6558dd81581815ceb804c53455
Yu-Lun <chou522128@gmail.com>
2023-03-18 22:26:43 +0800

    [Green] NormalPointComparer.cs：revise code
    revise First to FirstOrDefault and check repeatDices is not null

--- Sibala/Sibala/NormalPointComparer.cs
+++ Sibala/Sibala/NormalPointComparer.cs
@@ -10,11 +10,20 @@ namespace Sibala
         public string WinnerCategory => "normal point";
         public int Compare(List<Dice> player1Dices, List<Dice> player2Dices)
         {
-            var repeatDices1 = player1Dices.GroupBy(g => g.Value).OrderBy(o => o.Key).First(f => f.Count() == 2).ToList();
-            var repeatDices2 = player2Dices.GroupBy(g => g.Value).OrderBy(o => o.Key).First(f => f.Count() == 2).ToList();
 
-            var player1Point = player1Dices.Except(repeatDices1).Sum(s => s.Value);
-            var player2Point = player2Dices.Except(repeatDices2).Sum(s => s.Value);
+            var repeatDices1 = player1Dices.GroupBy(g => g.Value).OrderBy(o => o.Key).FirstOrDefault(f => f.Count() == 2);
+            var repeatDices2 = player2Dices.GroupBy(g => g.Value).OrderBy(o => o.Key).FirstOrDefault(f => f.Count() == 2);
+
+            var player1Point = 0;
+            var player2Point = 0;
+            if (repeatDices1 != null)
+            {
+                player1Point = player1Dices.Except(repeatDices1).Sum(s => s.Value);
+            }
+            if (repeatDices2 != null)
+            {
+                player2Point = player2Dices.Except(repeatDices2).Sum(s => s.Value);
+            }
 
             var compareDice = player1Point - player2Point;
 
