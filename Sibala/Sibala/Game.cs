773f64484dabab259e8497e0d9988615839b2508
Yu-Lun <chou522128@gmail.com>
2023-03-19 23:03:48 +0800

    [Refactor] Game.cs：implement Parse

--- Sibala/Sibala/Game.cs
+++ Sibala/Sibala/Game.cs
@@ -1,15 +1,19 @@
 ﻿
+using System.Linq;
+
 namespace Sibala
 {
     public class Game
     {
         public string ShowResult(string input)
         {
-            //var parse = new Parse();
-            //var parser = parse.Parser(input);
-            var winnerPlayer = "Black";
+            var parse = new Parse();
+            var parser = parse.Parser(input);
+
+            var winnerPlayer = parser[0].Name;
             var winnerCategory = "all of a kind";
-            var winnerPoint = "6";
+            var winnerPoint = parser[0].Dices.First().Output;
+
             return $"{winnerPlayer} win. - with {winnerCategory}: {winnerPoint}";
         }
     }
