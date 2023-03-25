994752185a4ce9c91d4337d1d8b276d93bd686df
Yu-Lun <chou522128@gmail.com>
2023-03-25 23:35:03 +0800

    [Refactor] Game.cs：add NormalPoint class

--- Sibala/Sibala/Game.cs
+++ Sibala/Sibala/Game.cs
@@ -18,11 +18,21 @@ namespace Sibala
             
             if (player1Dices.GroupBy(g => g.Value).Count(s => s.Count() == 2) > 0)
             {
-                return "Black win. - with normal point: 7";
+                var compare1 = new NormalPoint();
+                var compareResult1 = compare1.Compare(player1Dices, player2Dices);
+
+                if (compareResult1 != 0)
+                {
+                    var winnerPlayer = compareResult1 > 0 ? parser[0].Name : parser[1].Name;
+                    var winnerCategory = compare1.WinnerCategory;
+                    var winnerPoint = compare1.WinnerPoint;
+
+                    return $"{winnerPlayer} win. - with {winnerCategory}: {winnerPoint}";
+                }
             }
+
             var compare = new AllOfKind();
             var compareResult  = compare.Compare(player1Dices, player2Dices);
-            
 
             if (compareResult != 0)
             {
---
+++ Sibala/Sibala/NormalPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Sibala
+{
+    public class NormalPoint : AllOfKind
+    {
+        public string WinnerPoint { get; set; }
+        public string WinnerCategory => "normal point";
+
+        public int Compare(List<Dice> player1Dices, List<Dice> player2Dices)
+        {
+            var repeatDices1 = player1Dices.GroupBy(g => g.Value).OrderBy(o => o.Key).First(f => f.Count() == 2).ToList();
+            var repeatDices2 = player2Dices.GroupBy(g => g.Value).OrderBy(o => o.Key).First(f => f.Count() == 2).ToList();
+
+            var player1Point = player1Dices.Except(repeatDices1).Sum(s => s.Value);
+            var player2Point = player2Dices.Except(repeatDices2).Sum(s => s.Value);
+            
+            var compareDice = player1Point - player2Point;
+
+            if (compareDice != 0)
+            {
+                WinnerPoint = (compareDice > 0) ? player1Point.ToString() : player2Point.ToString();
+            }
+
+            return compareDice;
+        }
+    }
+}
