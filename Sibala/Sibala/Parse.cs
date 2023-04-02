3a521b2a1c0527ccfffe46c51f885ceda0cc2512
Yu-Lun <chou522128@gmail.com>
2023-04-02 23:33:05 +0800

    [Green] Parse.cs：split input string to get players dices

--- Sibala/Sibala/Parse.cs
+++ Sibala/Sibala/Parse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Sibala
 {
@@ -14,10 +15,15 @@ namespace Sibala
             foreach (var player in playerSection)
             {
                 var playerName = player.Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
+                var playerDices = player.Split(":", StringSplitOptions.RemoveEmptyEntries)[1]
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(dice => new Dices { Value = int.Parse(dice), Output = dice })
+                    .ToList();
 
                 playerInfo.Add(new Player
                 {
-                    Name = playerName
+                    Name = playerName,
+                    Dices = new List<Dices>(playerDices)
                 });
             }
 
