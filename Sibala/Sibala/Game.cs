6218db9c799f5990bc5626c8f10465ce57874ebf
Yu-Lun <chou522128@gmail.com>
2023-04-03 22:22:57 +0800

    [Green] Game.cs：check players point is the same
    if players point is the same  return Tie

--- Sibala/Sibala/Game.cs
+++ Sibala/Sibala/Game.cs
@@ -14,12 +14,17 @@ namespace Sibala
             var player1Point = parser[0].Dices.First();
             var player2Point = parser[1].Dices.First();
 
-            var comparePoint = player1Point.Value > player2Point.Value;
+            var comparePoint = player1Point.Value - player2Point.Value;
 
-            var winnerPlayer = comparePoint ? parser[0].Name : parser[1].Name;
-            var winnerCategory = "all of a kind";
-            var winnerPoint = comparePoint ? player1Point.Output : player2Point.Output;
-            return $"{winnerPlayer} win. - with {winnerCategory}: {winnerPoint}";
+            if (comparePoint != 0)
+            {
+                var winnerPlayer = player1Point.Value > player2Point.Value ? parser[0].Name : parser[1].Name;
+                var winnerCategory = "all of a kind";
+                var winnerPoint = player1Point.Value > player2Point.Value ? player1Point.Output : player2Point.Output;
+                return $"{winnerPlayer} win. - with {winnerCategory}: {winnerPoint}";
+            }
+
+            return "Tie";
         }
     }
 }
