77a56aa43c7cd631d33ca546a422f91c90fe29cc
Yu-Lun <chou522128@gmail.com>
2023-04-03 21:17:20 +0800

    [Green] Game.cs：compare players dices point
    compare players dices value, then get winner name and point

--- Sibala/Sibala/Game.cs
+++ Sibala/Sibala/Game.cs
@@ -11,9 +11,14 @@ namespace Sibala
             var parse = new Parse();
             var parser = parse.Parser(input);
 
-            var winnerPlayer = parser[0].Name;
+            var player1Point = parser[0].Dices.First();
+            var player2Point = parser[1].Dices.First();
+
+            var comparePoint = player1Point.Value > player2Point.Value;
+
+            var winnerPlayer = comparePoint ? parser[0].Name : parser[1].Name;
             var winnerCategory = "all of a kind";
-            var winnerPoint = parser[0].Dices.First().Output;
+            var winnerPoint = comparePoint ? player1Point.Output : player2Point.Output;
             return $"{winnerPlayer} win. - with {winnerCategory}: {winnerPoint}";
         }
     }
