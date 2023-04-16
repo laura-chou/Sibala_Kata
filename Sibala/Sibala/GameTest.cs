b0c6fd02b5b4302a17bc1a2dbdf03b6dde05d763
Yu-Lun <chou522128@gmail.com>
2023-04-16 22:30:54 +0800

    [Green] GameTest.cs - A01_BothNormalPoint：add new test case
    input：Black: 5 5 1 1  White: 1 2 1 4
    output：Black win. - with normal point: 10

--- Sibala/Sibala/GameTest.cs
+++ Sibala/Sibala/GameTest.cs
@@ -10,6 +10,7 @@ namespace Sibala
         [TestCase("Black: 2 5 3 3  White: 2 2 1 3", "Black win. - with normal point: 7")]
         [TestCase("Black: 5 3 5 4  White: 2 6 2 3", "White win. - with normal point: 9")]
         [TestCase("Black: 2 3 4 2  White: 1 1 4 3", "Tie")]
+        [TestCase("Black: 5 5 1 1  White: 1 2 1 4", "Black win. - with normal point: 10")]
         public void A01_BothNormalPoint(string input, string expected)
         {
             var game = new Game();
