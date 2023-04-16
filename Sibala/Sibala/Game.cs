ec45c9398e2dd2261b17ddf8c536115ea4b81773
Yu-Lun <chou522128@gmail.com>
2023-04-16 21:57:50 +0800

    [Refactor] Gmae.cs：implement Parser function and get player1 name and point

--- Sibala/Sibala/Game.cs
+++ Sibala/Sibala/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Sibala
 {
@@ -6,11 +7,25 @@ namespace Sibala
     {
         public string ShowResult(string input)
         {
-            //var parse = new Parse();
-            //var parser = parse.Parser(input);
-            var winnerPlayer = "Black";
+            var parse = new Parse();
+            var parser = parse.Parser(input);
+
+            var player1Name = parser[0].Name;
+            var player1Dices = parser[0].Dices;
+
+            var repeatDices1 = player1Dices
+                .GroupBy(dices => dices.Value)
+                .OrderBy(dice => dice.Key)
+                .First(dice => dice.Count() == 2)
+                .ToList();
+
+            var player1Point = player1Dices
+                .Except(repeatDices1)
+                .Sum(dice=> dice.Value);
+
+            var winnerPlayer = player1Name;
             var winnerCategory = "normal point";
-            var winnerPoint = "7";
+            var winnerPoint = player1Point;
             return $"{winnerPlayer} win. - with {winnerCategory}: {winnerPoint}";
         }
     }
