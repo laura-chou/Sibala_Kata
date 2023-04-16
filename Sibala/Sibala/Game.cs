e84761ffd6880757ce0b351c570e9e2a083cad1a
Yu-Lun <chou522128@gmail.com>
2023-04-16 22:06:37 +0800

    [Green] Game.cs：get player2 information and compare players point to get winner

--- Sibala/Sibala/Game.cs
+++ Sibala/Sibala/Game.cs
@@ -12,20 +12,31 @@ namespace Sibala
 
             var player1Name = parser[0].Name;
             var player1Dices = parser[0].Dices;
-
             var repeatDices1 = player1Dices
                 .GroupBy(dices => dices.Value)
                 .OrderBy(dice => dice.Key)
                 .First(dice => dice.Count() == 2)
                 .ToList();
-
             var player1Point = player1Dices
                 .Except(repeatDices1)
                 .Sum(dice=> dice.Value);
 
-            var winnerPlayer = player1Name;
+            var player2Name = parser[1].Name;
+            var player2Dices = parser[1].Dices;
+            var repeatDices2 = player2Dices
+                .GroupBy(dices => dices.Value)
+                .OrderBy(dice => dice.Key)
+                .First(dice => dice.Count() == 2)
+                .ToList();
+            var player2Point = player2Dices
+                .Except(repeatDices2)
+                .Sum(dice => dice.Value);
+
+            var compareResult = player1Point - player2Point;
+
+            var winnerPlayer = compareResult > 0 ? player1Name : player2Name;
             var winnerCategory = "normal point";
-            var winnerPoint = player1Point;
+            var winnerPoint = compareResult > 0 ? player1Point : player2Point;
             return $"{winnerPlayer} win. - with {winnerCategory}: {winnerPoint}";
         }
     }
