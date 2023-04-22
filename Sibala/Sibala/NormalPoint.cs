a053c4b9d9ac0ff07be3229b7fcb8a4e3e310e98
Yu-Lun <chou522128@gmail.com>
2023-04-22 22:25:18 +0800

    [Refactor] NormalPoint.cs：refactor code
    extract GetRepeatDice and GetPlayerPoint method

--- Sibala/Sibala/NormalPoint.cs
+++ Sibala/Sibala/NormalPoint.cs
@@ -11,23 +11,11 @@ namespace Sibala
 
         public int Compare(List<Dices> player1Dices, List<Dices> player2Dices)
         {
-            var repeatDices1 = player1Dices
-                 .GroupBy(dices => dices.Value)
-                 .OrderBy(dice => dice.Key)
-                 .First(dice => dice.Count() == 2)
-                 .ToList();
-            var player1Point = player1Dices
-                .Except(repeatDices1)
-                .Sum(dice => dice.Value);
+            var repeatDices1 = GetRepeatDice(player1Dices);
+            var player1Point = GetPlayerPoint(player1Dices, repeatDices1);
 
-            var repeatDices2 = player2Dices
-                .GroupBy(dices => dices.Value)
-                .OrderBy(dice => dice.Key)
-                .First(dice => dice.Count() == 2)
-                .ToList();
-            var player2Point = player2Dices
-                .Except(repeatDices2)
-                .Sum(dice => dice.Value);
+            var repeatDices2 = GetRepeatDice(player2Dices);
+            var player2Point = GetPlayerPoint(player2Dices, repeatDices2);
 
             var compareResult = player1Point - player2Point;
 
@@ -42,9 +30,25 @@ namespace Sibala
             if (compareResult != 0)
             {
                 WinnerPoint = compareResult > 0 ? player1Point.ToString() : player2Point.ToString();
-            } 
+            }
 
             return compareResult;
+        }
+
+        private int GetPlayerPoint(List<Dices> player1Dices, List<Dices> repeatDices1)
+        {
+            return player1Dices
+                .Except(repeatDices1)
+                .Sum(dice => dice.Value);
+        }
+
+        private List<Dices> GetRepeatDice(List<Dices> player1Dices)
+        {
+            return player1Dices
+                 .GroupBy(dices => dices.Value)
+                 .OrderBy(dice => dice.Key)
+                 .First(dice => dice.Count() == 2)
+                 .ToList();
         }
     }
 }
