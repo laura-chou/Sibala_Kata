d4f0f7f1d83e2c6c9ccc6beaa5524d3bad38be7d
Yu-Lun <chou522128@gmail.com>
2023-04-29 21:34:13 +0800

    [Red] GameTest.cs：add test case
    input：Black: 2 5 3 3  White: 2 2 1 3
    output：Black win. - with normal point: 7

---
+++ Sibala/Sibala/GameTest.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Sibala.src;
+
+namespace Sibala
+{
+    [TestFixture]
+    public class GameTest
+    {
+        [Test]
+        public void A01_BothNormalPoint()
+        {
+            var game = new Game();
+            var actual = game.ShowResult("Black: 2 5 3 3  White: 2 2 1 3");
+            actual.Should().Be("Black win. - with normal point: 7");
+        }
+    }
+}
