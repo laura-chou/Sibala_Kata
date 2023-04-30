1f4329995fb50973269b8892fb7fa69e1d356b27
Yu-Lun <chou522128@gmail.com>
2023-04-30 16:30:33 +0800

    [Refactor] GameTest.cs：add SetUp method and extract  AssertResultShouldReturn method

--- Sibala/Sibala/GameTest.cs
+++ Sibala/Sibala/GameTest.cs
@@ -7,12 +7,24 @@ namespace Sibala
     [TestFixture]
     public class GameTest
     {
+        Game _game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _game = new Game();
+        }
+
         [Test]
         [TestCase("Black: 2 5 3 3  White: 2 2 1 3", "Black win. - with normal point: 7")]
         public void A01_BothNormalPoint(string input, string expected)
         {
-            var game = new Game();
-            var actual = game.ShowResult(input);
+            AssertResultShouldReturn(input, expected);
+        }
+
+        private void AssertResultShouldReturn(string input, string expected)
+        {
+            var actual = _game.ShowResult(input);
             actual.Should().Be(expected);
         }
     }
