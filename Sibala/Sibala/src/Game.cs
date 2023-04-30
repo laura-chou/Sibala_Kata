a430ca2e1ba03d47e671c74c7863d8833901a7d4
Yu-Lun <chou522128@gmail.com>
2023-04-30 16:27:41 +0800

    [Refactor] Game.cs：implement Parse method

--- Sibala/Sibala/src/Game.cs
+++ Sibala/Sibala/src/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Sibala.src
 {
@@ -6,11 +7,22 @@ namespace Sibala.src
     {
         public string ShowResult(string input)
         {
-            //var parser = new Parser();
-            //var parse = parser.Parse(input);
-            var winnerPlayer = "Black";
+            var parser = new Parser();
+            var parse = parser.Parse(input);
+
+            var player1Dices = parse[0].Dices;
+
+            var player1repeatDices = player1Dices
+                .GroupBy(dices => dices.Value)
+                .OrderBy(dices => dices.Key)
+                .First(dice => dice.Count() == 2)
+                .ToList();
+
+            var player1Point = player1Dices.Except(player1repeatDices).Sum(dice => dice.Value);
+
+            var winnerPlayer = parse[0].Name;
             var winnerCategory = "normal point";
-            var winnerPoint = "7";
+            var winnerPoint = player1Point;
             return $"{winnerPlayer} win. - with {winnerCategory}: {winnerPoint}";
         }
     }
