5db56ebbd248f79b59af8796fab2f8da6b9f8446
Yu-Lun <chou522128@gmail.com>
2023-05-06 21:16:08 +0800

    [Green] Game.cs：count player1 repeat dice and hard code return

--- Sibala/Sibala/src/Game.cs
+++ Sibala/Sibala/src/Game.cs
@@ -13,12 +13,16 @@ namespace Sibala.src
             var player1Dices = parse[0].Dices;
             var player2Dices = parse[1].Dices;
             
+            if (player1Dices.GroupBy(dices => dices.Value).Count(dice => dice.Count() == 4) > 0)
+            {
+                return "Black win. - with all of a kind: 6";
+            }
+
             var compare = new NormalPoint();
             var compareResult = compare.Compare(player1Dices, player2Dices);
 
             if (compareResult != 0)
             {
-
                 var winnerPlayer = (compareResult > 0) ? parse[0].Name : parse[1].Name;
                 var winnerCategory = compare.WinnerCategory;
                 var winnerPoint = compare.WinnerPoint;
