85189dd567a1bc3ee304e73690d6eb7790828aa5
Yu-Lun <chou522128@gmail.com>
2023-05-07 22:11:31 +0800

    [Red] GameTest.cs - A01_BothAllOfKind：add test case
    input：Black: 6 6 6 6  White: 3 3 3 3
    output：Black win. - with all of a kind: 6

---
+++ Sibala/Sibala/GameTest.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Sibala.src;
+
+namespace Sibala
+{
+    [TestFixture]
+    public class GameTest
+    {
+        [Test]
+        public void A01_BothAllOfKind()
+        {
+            var game = new Game();
+            var actual = game.ShowResult("Black: 6 6 6 6  White: 3 3 3 3");
+            actual.Should().Be("Black win. - with all of a kind: 6");
+        }
+    }
+}
---
+++ Sibala/Sibala/src/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sibala.src
+{
+    public class Game
+    {
+        public string ShowResult(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
