89752f7ed82197d8b1d73f5098145bae7bdbba6f
Yu-Lun <chou522128@gmail.com>
2023-05-13 22:19:37 +0800

    [Green] Game.cs：compare players point and get winner

--- Sibala/Sibala/src/Game.cs
+++ Sibala/Sibala/src/Game.cs
@@ -10,9 +10,14 @@ namespace Sibala.src
             var parser = new Parser();
             var parse = parser.Parse(input);
 
-            var winnerPlayer = parse[0].Name;
+            var player1Point = parse[0].Dices.First();
+            var player2Point = parse[1].Dices.First();
+
+            var compare = player1Point.Value - player2Point.Value;
+
+            var winnerPlayer = compare > 0 ? parse[0].Name : parse[1].Name;
             var winnerCategory = "all of a kind";
-            var winnerPoint = parse[0].Dices.First().Output;
+            var winnerPoint = compare > 0 ? player1Point.Output : player2Point.Output;
             return $"{winnerPlayer} win. - with {winnerCategory}: {winnerPoint}";
         }
     }
