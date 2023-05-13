c38e172d9126b738bf3b643dacc3ef312b7fa871
Yu-Lun <chou522128@gmail.com>
2023-05-13 21:08:03 +0800

    [Refactor] Parser.cs：extract GetPlayer method and refactor code

--- Sibala/Sibala/src/Parser.cs
+++ Sibala/Sibala/src/Parser.cs
@@ -9,40 +9,29 @@ namespace Sibala.src
         public List<Player> Parse(string input)
         {
             var playerSection = input.Split("  ", StringSplitOptions.RemoveEmptyEntries);
-            var player1Name = playerSection[0].Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
-            var player1Dices = playerSection[0]
-                .Split(":", StringSplitOptions.RemoveEmptyEntries)[1]
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                .Select(dice => new Dices
-                {
-                    Value = int.Parse(dice),
-                    Output = dice
-                })
-                .ToList();
-            
-            var player2Name = playerSection[1].Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
-            var player2Dices = playerSection[1]
-                .Split(":", StringSplitOptions.RemoveEmptyEntries)[1]
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                .Select(dice => new Dices
-                {
-                    Value = int.Parse(dice),
-                    Output = dice
-                })
-                .ToList();
 
-            return new List<Player>
+            return playerSection
+                .Select(section => GetPlayer(section))
+                .ToList();
+        }
+
+        private Player GetPlayer(string playerSection)
+        {
+            var playerName = playerSection.Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
+            var playerDices = playerSection
+                            .Split(":", StringSplitOptions.RemoveEmptyEntries)[1]
+                            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                            .Select(dice => new Dices
+                            {
+                                Value = int.Parse(dice),
+                                Output = dice
+                            })
+                            .ToList();
+
+            return new Player
             {
-                new Player
-                {
-                    Name = player1Name,
-                    Dices = new List<Dices>(player1Dices)
-                },
-                new Player
-                {
-                    Name = player2Name,
-                    Dices = new List<Dices>(player2Dices)
-                }
+                Name = playerName,
+                Dices = new List<Dices>(playerDices)
             };
         }
     }
