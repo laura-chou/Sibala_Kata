9dc847ff7cf9d32a3fd825d2c3953ca159b246b2
Yu-Lun <chou522128@gmail.com>
2023-06-17 22:12:53 +0800

    [Green] Game.cs：hard code return

--- Sibala/Sibala/src/Game.cs
+++ Sibala/Sibala/src/Game.cs
@@ -6,7 +6,7 @@ namespace Sibala.src
     {
         public string ShowResult(string input)
         {
-            throw new NotImplementedException();
+            return "Black win. - with all of a kind: 6";
         }
     }
 }
