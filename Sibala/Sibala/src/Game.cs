67ea939d058ed12becf1a59841bb03d5b34b8e29
Yu-Lun <chou522128@gmail.com>
2023-06-17 22:30:38 +0800

    [Refactor] Game.cs：return string replace to variable

--- Sibala/Sibala/src/Game.cs
+++ Sibala/Sibala/src/Game.cs
@@ -6,7 +6,10 @@ namespace Sibala.src
     {
         public string ShowResult(string input)
         {
-            return "Black win. - with all of a kind: 6";
+            var winnerPlayer = "Black";
+            var winnerCategory = "all of a kind";
+            var winnerPoint = "6";
+            return $"{winnerPlayer} win. - with {winnerCategory}: {winnerPoint}";
         }
     }
 }
