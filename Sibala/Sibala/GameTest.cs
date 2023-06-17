41859ab62f114ad7faf4d605c8f8e42317ac4825
Yu-Lun <chou522128@gmail.com>
2023-06-17 22:23:11 +0800

    [Refactor] GameTest.cs：use SetUp and extract AssertResultShouldReturn method

--- Sibala/Sibala/GameTest.cs
+++ Sibala/Sibala/GameTest.cs
@@ -7,12 +7,24 @@ namespace Sibala
     [TestFixture]
     public class GameTest
     {
+        private Game _game;
+
+        [SetUp]
+        public void Setup()
+        {
+            _game = new Game();
+        }
+
         [Test]
         public void A01_BothAllOfKind()
         {
-            var game = new Game();
-            var actual = game.ShowResult("Black: 6 6 6 6  White: 3 3 3 3");
-            actual.Should().Be("Black win. - with all of a kind: 6");
+            AssertResultShouldReturn("Black: 6 6 6 6  White: 3 3 3 3", "Black win. - with all of a kind: 6");
+        }
+
+        private void AssertResultShouldReturn(string input, string expected)
+        {
+            var actual = _game.ShowResult(input);
+            actual.Should().Be(expected);
         }
     }
 }
