0a244ef7d2f742967ce3dcc214c63b14243627c1
Yu-Lun <chou522128@gmail.com>
2023-06-18 21:51:48 +0800

    [Red] ParserTest.cs - A01_Parse：player class add Dices property

--- Sibala/Sibala/ParserTest.cs
+++ Sibala/Sibala/ParserTest.cs
@@ -17,11 +17,25 @@ namespace Sibala
             {
                 new Player
                 { 
-                    Name = "Black" 
+                    Name = "Black",
+                    Dices = new List<Dice>
+                    {
+                        new Dice { Value = 6, Output = "6" },
+                        new Dice { Value = 6, Output = "6" },
+                        new Dice { Value = 6, Output = "6" },
+                        new Dice { Value = 6, Output = "6" }
+                    }
                 },
                 new Player
                 {
-                    Name = "White"
+                    Name = "White",
+                    Dices = new List<Dice>
+                    {
+                        new Dice { Value = 3, Output = "3" },
+                        new Dice { Value = 3, Output = "3" },
+                        new Dice { Value = 3, Output = "3" },
+                        new Dice { Value = 3, Output = "3" }
+                    }
                 }
             };
             actual.Should().BeEquivalentTo(expected);
---
+++ Sibala/Sibala/src/Dice.cs
@@ -0,0 +1,8 @@
+﻿namespace Sibala.src
+{
+    public class Dice
+    {
+        public int Value { get; set; }
+        public string Output { get; set; }
+    }
+}
