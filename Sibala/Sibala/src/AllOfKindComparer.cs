7638b472193aacc3075a39bda5dea974abfa0e3b
Yu-Lun <chou522128@gmail.com>
2023-06-23 22:24:59 +0800

    [Refactor] Game.cs：move Compare method to AllOfKindComparer class

---
+++ Sibala/Sibala/src/AllOfKindComparer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Sibala.src
+{
+    public class AllOfKindComparer
+    {
+        public int Compare(List<Dice> player1Dices, List<Dice> player2Dices, out string winnerPoint)
+        {
+            var compareResult = player1Dices.First().Value - player2Dices.First().Value;
+            winnerPoint = string.Empty;
+            if (compareResult != 0)
+            {
+                winnerPoint = compareResult > 0 ? player1Dices.First().Output : player2Dices.First().Output;
+            }
+            return compareResult;
+        }
+    }
+}
--- Sibala/Sibala/src/Game.cs
+++ Sibala/Sibala/src/Game.cs
@@ -15,7 +15,8 @@ namespace Sibala.src
             var player1Dices = parse[0].Dices;
             var player2Dices = parse[1].Dices;
 
-            int compareResult = Compare(player1Dices, player2Dices, out var winnerPoint);
+            var comparer = new AllOfKindComparer();
+            int compareResult = comparer.Compare(player1Dices, player2Dices, out var winnerPoint);
             
             if (compareResult != 0)
             {
@@ -25,17 +26,6 @@ namespace Sibala.src
             }
 
             return "Tie";
-        }
-
-        private int Compare(List<Dice> player1Dices, List<Dice> player2Dices, out string winnerPoint)
-        {
-            var compareResult = player1Dices.First().Value - player2Dices.First().Value;
-            winnerPoint = string.Empty;
-            if (compareResult != 0)
-            {
-                winnerPoint = compareResult > 0 ? player1Dices.First().Output : player2Dices.First().Output;
-            }
-            return compareResult;
         }
     }
 }
