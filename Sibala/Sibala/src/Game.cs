44d9aec78ca57c4e7b79b1be1598daa8a3a2b7de
Yu-Lun <chou522128@gmail.com>
2023-06-25 22:12:27 +0800

    [Refactor] Game.cs：rename variable parse to players

--- Sibala/Sibala/src/Game.cs
+++ Sibala/Sibala/src/Game.cs
@@ -10,17 +10,17 @@ namespace Sibala.src
         public string ShowResult(string input)
         {
             var parser = new Parser();
-            var parse = parser.Parse(input);
+            var players = parser.Parse(input);
 
-            var player1Dices = parse[0].Dices;
-            var player2Dices = parse[1].Dices;
+            var player1Dices = players[0].Dices;
+            var player2Dices = players[1].Dices;
 
             var comparer = new AllOfKindComparer();
             int compareResult = comparer.Compare(player1Dices, player2Dices);
             
             if (compareResult != 0)
             {
-                var winnerPlayer = compareResult > 0 ? parse[0].Name : parse[1].Name;
+                var winnerPlayer = compareResult > 0 ? players[0].Name : players[1].Name;
                 var winnerCategory = comparer.WinnerCategory;
                 var winnerPoint = comparer.WinnerPoint;
                 return $"{winnerPlayer} win. - with {winnerCategory}: {winnerPoint}";
