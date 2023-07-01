1dd41c90451bb5ea89d24c920bff4bb823bb6077
Yu-Lun <chou522128@gmail.com>
2023-07-01 22:05:17 +0800

    [Refactor] NormalPointComparer.cs：extract CalculateNormalPoint method

--- Sibala/Sibala/src/NormalPointComparer.cs
+++ Sibala/Sibala/src/NormalPointComparer.cs
@@ -11,23 +11,24 @@ namespace Sibala.src
 
         public int Compare(List<Dice> player1Dices, List<Dice> player2Dices)
         {
-            var repeatDices1 = player1Dices
-                .GroupBy(dices => dices.Value)
-                .First(dice => dice.Count() == 2)
-                .ToList();
-            var repeatDices2 = player2Dices
-                .GroupBy(dices => dices.Value)
-                .First(dice => dice.Count() == 2)
-                .ToList();
-
-            var player1Point = player1Dices.Except(repeatDices1).Sum(dice => dice.Value);
-            var player2Point = player2Dices.Except(repeatDices2).Sum(dice => dice.Value);
+            var player1Point = CalculateNormalPoint(player1Dices);
+            var player2Point = CalculateNormalPoint(player2Dices);
 
             var compareResult = player1Point - player2Point;
 
             WinnerPoint = compareResult > 0 ? player1Point.ToString() : player2Point.ToString();
 
             return compareResult;
+        }
+
+        private static int CalculateNormalPoint(List<Dice> playerDices)
+        {
+            var repeatDices = playerDices
+                .GroupBy(dices => dices.Value)
+                .First(dice => dice.Count() == 2)
+                .ToList();
+
+            return playerDices.Except(repeatDices).Sum(dice => dice.Value);
         }
     }
 }
