cbe8296f9e129e233925c1c16bb771746845c5c0
Yu-Lun <chou522128@gmail.com>
2023-07-05 23:30:30 +0800

    [Refactor] Game.cs：compare players dices category and DifferentCategoryComparer class hard code return

---
+++ Sibala/Sibala/src/DifferentCategoryComparer.cs
@@ -0,0 +1,17 @@
+﻿namespace Sibala.src
+{
+    internal class DifferentCategoryComparer : IComparer
+    {
+        public string WinnerCategory { get; private set; }
+
+        public string WinnerPoint { get; private set; }
+
+        public int Compare(Dices player1Dices, Dices player2Dices)
+        {
+            WinnerCategory = "all of a kind";
+            WinnerPoint = "6";
+
+            return 1;
+        }
+    }
+}
