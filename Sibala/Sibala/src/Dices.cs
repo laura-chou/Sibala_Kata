0bb575397dad3316d7ceb0f462f7a0ee1a7d463a
Yu-Lun <chou522128@gmail.com>
2023-07-05 22:54:33 +0800

    [Refactor] Game.cs：move GetDicesCategoryType method  to Dices class

--- Sibala/Sibala/src/Dices.cs
+++ Sibala/Sibala/src/Dices.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Sibala.src
 {
@@ -10,6 +12,24 @@ namespace Sibala.src
         public Dices(List<Dice> dice)
         {
             _dice = dice;
+        }
+
+        public CategoryType GetDicesCategoryType()
+        {
+            var diceGrouping = this.GroupBy(dices => dices.Value).ToList();
+            if (diceGrouping
+                .Count(dice => dice.Count() == 4) == 1)
+            {
+                return CategoryType.AllOfKind;
+            }
+
+            if (diceGrouping
+                .Count(dice => dice.Count() == 2) >= 1)
+            {
+                return CategoryType.NormalPoint;
+            }
+            
+            throw new NotImplementedException();
         }
 
         public IEnumerator<Dice> GetEnumerator()
