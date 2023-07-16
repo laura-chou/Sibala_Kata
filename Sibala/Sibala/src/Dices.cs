9a205adfc4caa81a00e79d44cacc8b0afcfaeb53
Yu-Lun <chou522128@gmail.com>
2023-07-16 22:05:19 +0800

    [Refactor] Game.cs：GetDicesCategoryType parameter change to Dices class

---
+++ Sibala/Sibala/src/Dices.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Sibala.src
+{
+    internal class Dices : IEnumerable<Dice>
+    {
+        private readonly List<Dice> _dice;
+
+        public Dices (List<Dice> dice)
+        {
+            _dice = dice;
+        }
+
+        public IEnumerator<Dice> GetEnumerator()
+        {
+            return _dice.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- Sibala/Sibala/src/Game.cs
+++ Sibala/Sibala/src/Game.cs
@@ -15,7 +15,7 @@ namespace Sibala.src
             var player1Dices = players[0].Dices;
             var player2Dices = players[1].Dices;
 
-            var dice1CategoryType = GetDicesCategoryType(player1Dices);
+            var dice1CategoryType = GetDicesCategoryType(new Dices(player1Dices));
 
             IComparer comparer;
 
@@ -41,7 +41,7 @@ namespace Sibala.src
             return "Tie";
         }
 
-        private static CategoryType GetDicesCategoryType(List<Dice> playerDices)
+        private static CategoryType GetDicesCategoryType(Dices playerDices)
         {
             if (playerDices
                 .GroupBy(dices => dices.Value)
