aef85b4f7e3bb1b44f75d7ac30df657b53ff0e1e
Yu-Lun <chou522128@gmail.com>
2023-07-23 21:27:09 +0800

    [Refactor] Game.cs：winner description get from comparer.WinnerCategroy

--- Sibala/Sibala/src/Categories/Category.cs
+++ Sibala/Sibala/src/Categories/Category.cs
@@ -2,6 +2,7 @@
 {
     public abstract class Category
     {
+        public string Description => $"{Name}: {Output}";
         public abstract string Name { get; }
         public abstract string Output { get; }
         public abstract CategoryType Type { get; }
