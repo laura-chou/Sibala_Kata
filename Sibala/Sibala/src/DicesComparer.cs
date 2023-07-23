6a5fed15a64c919e1c33ed544e939db8bf403969
Yu-Lun <chou522128@gmail.com>
2023-07-23 21:49:45 +0800

    [Refactor] Game.cs：add DicesComparer class and assign to comparer

---
+++ Sibala/Sibala/src/DicesComparer.cs
@@ -0,0 +1,43 @@
+﻿using Sibala.src.Categories;
+using System.Collections.Generic;
+
+namespace Sibala.src
+{
+    public class DicesComparer : IComparer
+    {
+        public Category WinnerCategory { get; private set; }
+
+        public int Compare(Dices player1Dices, Dices player2Dices)
+        {
+            IComparer comparer;
+            
+            var dice1CategoryType = player1Dices.GetCategory().Type;
+            var dice2CategoryType = player2Dices.GetCategory().Type;
+            
+            if (dice1CategoryType != dice2CategoryType)
+            {
+                comparer = new DifferentCategoryComparer();
+            }
+            else
+            {
+                var categoryComparerMapper = new Dictionary<CategoryType, IComparer>
+                {
+                    { CategoryType.NormalPoint, new NormalPointComparer() },
+                    { CategoryType.AllOfKind, new AllOfKindComparer() },
+                    { CategoryType.NoPoint, new NoPointComparer() }
+                };
+
+                comparer = categoryComparerMapper[dice1CategoryType];
+            }
+
+            var compareResult = comparer.Compare(player1Dices, player2Dices);
+
+            if (compareResult != 0)
+            {
+                WinnerCategory = compareResult > 0 ? player1Dices.GetCategory() : player2Dices.GetCategory();
+            }
+
+            return compareResult;
+        }
+    }
+}
--- Sibala/Sibala/src/Game.cs
+++ Sibala/Sibala/src/Game.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestPlatform.Utilities;
+using NUnit.Framework.Constraints;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,27 +16,7 @@ namespace Sibala.src
             var player1Dices = players[0].Dices;
             var player2Dices = players[1].Dices;
 
-            var dice1CategoryType = player1Dices.GetCategory().Type;
-            var dice2CategoryType = player2Dices.GetCategory().Type;
-
-            IComparer comparer;
-
-            if (dice1CategoryType != dice2CategoryType)
-            {
-                comparer = new DifferentCategoryComparer();
-            }
-            else
-            {
-                var categoryComparerMapper = new Dictionary<CategoryType, IComparer>
-                {
-                    { CategoryType.NormalPoint, new NormalPointComparer() },
-                    { CategoryType.AllOfKind, new AllOfKindComparer() },
-                    { CategoryType.NoPoint, new NoPointComparer() }
-                };
-
-                comparer = categoryComparerMapper[dice1CategoryType];
-            }
-            
+            var comparer = new DicesComparer();
 
             var compareResult = comparer.Compare(player1Dices, player2Dices);
 
